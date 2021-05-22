f01141a534411d0c35acfba942e54a10cf933d7c
Ltotheuhas <lkokorovic@hotmail.com>
2021-05-22 15:01:17 +0200

    added everything

--- index.html
+++ index.html
@@ -1,9 +1,36 @@
-<!doctype html>
-<html>
-  <head>
-    <title>This is the title of the webpage!</title>
-  </head>
-  <body>
-    <p>This is an example paragraph. Anything in the <strong>body</strong> tag will appear on the page, just like this <strong>p</strong> tag and its contents.</p>
-  </body>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <link href="assets/favicon.ico" rel="icon" type="image/x-icon" />
+    <title>The site of Luhas</title>
+</head>
+<body>
+<section class="showcase" style="padding: 0;">
+    <a id="btn" href="home.html"></a>
+    <video id="homepage" autoplay muted playsinline></video>
+</section>
+</body>
+<script>
+    function randomInteger(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+    function setScreen() {
+        switch (randomInteger(1, 3)) {
+            case 1:
+                document.getElementById("homepage").src = "assets/homepage1.mp4";
+                break;
+            case 2:
+                document.getElementById("homepage").src = "assets/homepage2.mp4";
+                break;
+            case 3:
+                document.getElementById("homepage").src = "assets/homepage3.mp4";
+                break;
+        }
+    }
+    window.onload = setScreen;
+</script>
 </html>
---
+++ js/main.js
@@ -0,0 +1,80 @@
+//disable image interaction / right click
+$('img').on('dragstart', function(event) { event.preventDefault(); });
+$("body").on("contextmenu", "img", function(e) {
+    return false;
+});
+
+//fluctuating text content on the eva page
+var texts = new Array();
+texts.push("Eva Hakai is an internet music label");
+texts.push("Eva Hakai is a creative collective");
+texts.push("Eva Hakai is a physical music distributor");
+texts.push("Eva Hakai is an internet subordinate");
+texts.push("Eva Hakai is a reaction to the current musical zeitgeist");
+texts.push("Eva Hakai is for the online era avant-teens");
+texts.push("Eva Hakai is not dead");
+texts.push("Eva Hakai is a clothing brand");
+texts.push("Eva Hakai is something to represent");
+texts.push("Eva Hakai is ﷽");
+texts.push("Eva Hakai is a loose mesh of musicians");
+texts.push("Eva Hakai is a ton of work");
+texts.push("Eva Hakai is cooler than your average net label");
+texts.push("Eva Hakai is a part of history");
+
+var point = 0;
+
+function changeText() {
+    $('#evaIs').html(texts[point]);
+    if (point < (texts.length - 1)) {
+        point++;
+    } else {
+        point = 0;
+    }
+
+}
+
+setInterval(changeText, 50); /*Call it here*/
+changeText();
+
+//image deaggable
+$(".draggable").draggable();
+
+//epic cursor button
+var currentCursor = null;
+var newCursor = null;
+
+function randomInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function checkNonduplicateCursor() {
+    while (currentCursor == newCursor) {
+        newCursor = randomInteger(1, 5);
+    }
+    currentCursor = newCursor;
+    console.log(currentCursor);
+    return currentCursor;
+}
+
+function change() {
+    switch (checkNonduplicateCursor()) {
+        case 1:
+            document.body.style.cursor = "url(assets/cursors/sword.gif),auto";
+            break;
+        case 2:
+            document.body.style.cursor = "url(assets/cursors/asuka.gif),auto";
+            break;
+        case 3:
+            document.body.style.cursor = "url(assets/cursors/miku.gif),auto";
+            break;
+        case 4:
+            document.body.style.cursor = "url(assets/cursors/sanrio.gif),auto";
+            break;
+        case 5:
+            document.body.style.cursor = "url(assets/cursors/tsukasa.gif),auto";
+            break;
+    }
+}
+
+
+
