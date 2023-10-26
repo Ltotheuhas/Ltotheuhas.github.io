2a5f147da47484481905cb2f5c43bd21c9a37c1a
Ltotheuhas <lkokorovic@hotmail.com>
2023-10-26 16:58:48 +0200

    vue test
    dont think tjis will workl

---
+++ src/views/HomeView.vue
@@ -0,0 +1,35 @@
+<template>
+  <RandomImage :numImages="randomNumImages" style="position: fixed; "/>
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue';
+
+// Components
+import RandomImage from '../components/RandomImage.vue';
+
+export default defineComponent({
+  name: 'HomeView',
+
+  components: {
+    RandomImage,
+  },
+
+  setup() {
+    const randomNumImages = ref(1); // Initialize with 1 image
+
+    const generateRandomNumImages = () => {
+      const min = 1;
+      const max = 50;
+      randomNumImages.value = Math.floor(Math.random() * (max - min + 1)) + min;
+    };
+
+    generateRandomNumImages();
+
+    return {
+      randomNumImages,
+      generateRandomNumImages,
+    };
+  },
+});
+</script>
